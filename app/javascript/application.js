406e25c9684b82346298f2732eb73db656c75b58
Zoe Picone <zoe@zoepicone.dev>
2022-12-12 09:58:05 +1300

    Added Carbon web components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@carbon/web-components/dist/button.min.js"
+import "@carbon/web-components/dist/link.min.js"
+import "@carbon/web-components/dist/ui-shell.min.js"
