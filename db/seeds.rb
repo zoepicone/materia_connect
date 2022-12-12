7e2cbc7cf2e6cb42cf1af027c529f0a402a4162e
Zoe Picone <zoe@zoepicone.dev>
2022-12-12 21:20:57 +1300

    Added data seeds, made sure users can't edit mods that aren't theirs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+mod1 = Mod.create!(title: 'Mod 1',
+                   description: 'This is the first mod.',
+                   tags: 'mod, first, body',
+                   download_url: 'https://youtube.com/watch?v=dQw4w9WgXcQ',
+                   user_id: 1)
+
+mod2 = Mod.create!(title: 'Mod 2',
+                   description: 'This is the second mod.',
+                   tags: 'mod, second, clothing',
+                   download_url: 'https://youtube.com/watch?v=dQw4w9WgXcQ',
+                   user_id: 1)
