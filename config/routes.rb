b0e8b91b3aff9b4188f5a490beefe1cca0bf18f8
Zoe Picone <zoe@zoepicone.dev>
2022-12-12 14:38:10 +1300

    Fix match, again

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   resources :mods
 
   get '/login', to: 'sessions#new', as: :login
-  match 'auth/:provider/callback', to: 'sessions#create'
-  match '/auth/failure', to: 'sessions#failure'
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match '/auth/failure', to: 'sessions#failure', via: [:get, :post]
   get '/logout', :to => 'sessions#destroy', :as => :logout
 
 end
